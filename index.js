346f11b0533dc5e3676333d289cb0eb52186ac4a
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-23 00:08:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://kasarschetan1122:Yhcs4f5RBWwKqmU2@cluster13.x1rw8bv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+const cors = require('cors');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/add', async (req, res) => {
+
+    const database = client.db("mydb2");
+    const mycollection = database.collection("mycollection");
+    const result = await mycollection.insertOne(req.body.userData);
+    console.log(req.body.userData);
+
+    // console.log(global.imageName);
+
+    //res.json({ message: 'Data received on the server' });
+});
+
 
 app.use("/",(req, res)=>{
   res.json({message:"Hello"});
