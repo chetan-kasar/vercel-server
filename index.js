431c8f3e90d9fa36b1543e8170a98297156db276
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-23 18:03:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://kasarschetan1122:yuv3XpgeovmKFRFU@cluster07.ma9cvqv.mongodb.net/?retryWrites=true&w=majority";
-
 const client = new MongoClient(uri);
 const app = express();
 const cors = require('cors');
@@ -18,34 +17,13 @@ app.use("/home",(req,res)=>{
   res.send("Home is working");
 });
 
-app.use("/add",async(req, res) => {
-  try {
-        // Connect to the Atlas cluster
-         await client.connect();
-         const db = client.db(dbName);
-         // Reference the "people" collection in the specified database
-         const col = db.collection("people");
-         // Create a new document                                                                                                                                           
-         let personDocument = {
-             "name": { "first": "Alan", "last": "Turing" },
-             "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
-             "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
-             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
-             "views": 1250000
-         }
-         // Insert the document into the specified collection        
-         const p = await col.insertOne(personDocument);
-         // Find and return the document
-         const filter = { "name.last": "Turing" };
-         const document = await col.findOne(filter);
-         console.log("Document found:\n" + JSON.stringify(document));
-        } catch (err) {
-         console.log(err.stack);
-     }
- 
-     finally {
-        await client.close();
-    }
+app.use('/add', async (req, res) => {
+  console.log("add is working");
+  const uri = "mongodb+srv://kasarschetan1122:yuv3XpgeovmKFRFU@cluster07.ma9cvqv.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  const database = client.db("mydb3");
+  const mycollection = database.collection("mycollection");
+  const result = await mycollection.insertOne(req.body.userData);
 });
 
 const PORT = 9000;
