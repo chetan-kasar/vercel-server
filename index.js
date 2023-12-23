63a393381d9780ced249f4df18b6e109ccae09e2
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-23 14:09:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const { MongoClient } = require('mongodb');
-//const uri = "mongodb+srv://kasarschetan1122:D7Ybtd5WfHapo4tV@cluster2.3fczggr.mongodb.net/?retryWrites=true&w=majority";
-//const client = new MongoClient(uri);
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://kasarschetan1122:NCRXzlI79epw381J@cluster3.oadis6o.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 const app = express();
 const cors = require('cors');
 app.use(cors());
@@ -14,9 +14,9 @@ app.use("/home",(req,res)=>{
 
 app.post("/add",(req, res) => {
   res.send("add is working");
-  //const database = client.db("mydb3");
-  //const mycollection = database.collection("mycollection");
-  //const result = await mycollection.insertOne(req.body.userData);
+  const database = client.db("mydb3");
+  const mycollection = database.collection("mycollection");
+  const result = await mycollection.insertOne(req.body.userData);
 });
 
 const PORT = 9000;
