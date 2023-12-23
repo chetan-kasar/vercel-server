e56dde1332462707b08432358d252ce50475722e
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-23 11:39:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://kasarschetan1122:D7Ybtd5WfHapo4tV@cluster2.3fczggr.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
+//const { MongoClient } = require('mongodb');
+//const uri = "mongodb+srv://kasarschetan1122:D7Ybtd5WfHapo4tV@cluster2.3fczggr.mongodb.net/?retryWrites=true&w=majority";
+//const client = new MongoClient(uri);
 const app = express();
 const cors = require('cors');
 app.use(cors(
@@ -18,10 +18,10 @@ app.use("/",(req,res)=>{
 });
 
 app.post('/add', async (req, res) => {
-  res.json({message:"In add"});
-  const database = client.db("mydb3");
-  const mycollection = database.collection("mycollection");
-  const result = await mycollection.insertOne(req.body.userData);
+  res.send("received");
+  //const database = client.db("mydb3");
+  //const mycollection = database.collection("mycollection");
+  //const result = await mycollection.insertOne(req.body.userData);
 });
 
 const PORT = 9000;
