f485ba89d1259a68dbee599d43861b30f9f45fef
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-23 23:55:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "*",
+  origin: "https://finalfk.vercel.app",
   methods: ['GET','POST'],
   credentials: true 
 }));
